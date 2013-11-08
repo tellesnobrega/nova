be158fb9646390b7d22b6e0e585950b407ac8f24
Raildo Mascena <raildo@lsd.ufcg.edu.br>
2013-11-08 16:50:24 -0300

    fix bugs

--- nova/tests/network/test_manager.py
+++ nova/tests/network/test_manager.py
@@ -1180,7 +1180,8 @@ class VlanNetworkTestCase(test.TestCase):
         self.network._floating_ip_owned_by_project(ctxt, floating_ip)
 
     def test_allocate_floating_ip(self):
-        ctxt = context.RequestContext('testuser', 'testproject',
+        ctxt = context.RequestContext('testuser', 'testproject', 
+                                      'testdomain',
                                       is_admin=False)
 
         def fake_allocate_address(*args, **kwargs):

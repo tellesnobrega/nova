93dea2f9282741e491aecccd79f36c81c7b4e9f7
Telles Nobrega <tellesmvn@lsd.ufcg.edu.br>
2013-11-18 10:17:15 -0300

    Query test

--- nova/db/sqlalchemy/api.py
+++ nova/db/sqlalchemy/api.py
@@ -227,10 +227,6 @@ def model_query(context, model, *args, **kwargs):
 
     query = session.query(model, *args)
 
-    print "<<<<<<<<<<<<<<<<<<<<1.QUERY>>>>>>>>>>>>>>>>>>>"
-    print query
-    print "<<<<<<<<<<<<<<<<<<<<1.QUERY>>>>>>>>>>>>>>>>>>>"
-
     default_deleted_value = base_model.__mapper__.c.deleted.default.arg
 
     if read_deleted == 'no':
@@ -242,10 +238,6 @@ def model_query(context, model, *args, **kwargs):
     else:
         raise Exception(_("Unrecognized read_deleted value '%s'")
                             % read_deleted)
-
-    print "<<<<<<<<<<<<<<<<<<<<2.QUERY>>>>>>>>>>>>>>>>>>>"
-    print query
-    print "<<<<<<<<<<<<<<<<<<<<2.QUERY>>>>>>>>>>>>>>>>>>>"
 
     if nova.context.is_user_context(context) and project_only:
         if project_only == 'allow_none':
@@ -2962,15 +2954,19 @@ def quota_get_all_by_project_and_user(context, project_id, user_id):
 def quota_get_all_by_domain(context, domain_id):
     nova.context.authorize_domain_context(context, domain_id)
 
-    rows = model_query(context, models.DomainQuota, read_deleted="no").\
-                   filter_by(domain_id=domain_id).\
-                   all()
+    query = model_query(context, models.DomainQuota, read_deleted="no")
+    print "<<<<<<<<<<<<<<<<<<<<<<<QUERY>>>>>>>>>>>>>>>>>>>>>"
+    print query
+    print "<<<<<<<<<<<<<<<<<<<<<<<QUERY>>>>>>>>>>>>>>>>>>>>>"
+
+    rows = query.filter_by(domain_id=domain_id).all()
+    print "<<<<<<<<<<<<<<<<<<<<<<<<ROWS>>>>>>>>>>>>>>>>>>>>>"
+    print rows
+    print "<<<<<<<<<<<<<<<<<<<<<<<<ROWS>>>>>>>>>>>>>>>>>>>>>"
+
 
     result = {'domain_id': domain_id}
     for row in rows:
-        print "<<<<<<<<<<<<<<<<<<<<<ROW>>>>>>>>>>>>>>>>>>"
-        print row
-        print "<<<<<<<<<<<<<<<<<<<<<ROW>>>>>>>>>>>>>>>>>>"
         result[row.resource] = row.hard_limit
 
     print "<<<<<<<<<<<<<<<<DOMAIN_QUOTAS_FROM_DB>>>>>>>>>>>>>>>>>>>>>>>>>>"
